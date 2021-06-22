a4a9e2b9d2e3187980ffb7b7f2fc958e7f320972
wander4747 <wander.douglas14@gmail.com>
2021-06-22 20:27:33 -0300

    add pat router

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,9 +28,13 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber, nil)
+
+	server := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = server.ListenAndServe()
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wander4747/booking
 
 go 1.16
 
-require github.com/go-delve/delve v1.6.1
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+	github.com/go-delve/delve v1.6.1
+)
