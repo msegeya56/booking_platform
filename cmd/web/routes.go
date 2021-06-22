3d55cf01463e3bd3f203bc6d83ab8f73bf7ad5b5
wander4747 <wander.douglas14@gmail.com>
2021-06-22 20:48:37 -0300

    add chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/wander4747/booking/pkg/config"
 	"github.com/wander4747/booking/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/wander4747/booking
 
 go 1.16
 
-require (
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
-	github.com/go-delve/delve v1.6.1
-)
+require github.com/go-chi/chi/v5 v5.0.3
